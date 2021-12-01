8db35b9146052060378b37ffdf3640013ad9d79f
Noah Guillory <github@noahguillory.me>
2021-12-01 19:15:58 +0000

    Solve D1P1

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,15 @@
 
 pub fn part1(inp: String) {
+    let mut changes = 0;
+    let mut last_measurement: i32 = -1;
+    let measurements: Vec<i32> = inp.lines().into_iter().map(|s| s.parse::<i32>().unwrap()).collect();
 
+    for measurement in measurements.iter() {
+      if measurement > &last_measurement && last_measurement != -1 {
+        changes += 1;
+      }
+      last_measurement = *measurement;
+    }
+
+    println!("{}", changes);
 }
